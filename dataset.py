640fb3c14dadff79873087407305eaa2be042630
yli4924 <yli4924@gmail.com>
2023-01-10 14:04:57 -0500

    create dataset.py to read dataset

--- dataset.py
+++ dataset.py
@@ -1,1 +1,9 @@
+from torch.utils.data import Dataset
+from torchtext.data.utils import get_tokenizer
+
+class MovieDataset(Dataset):
+    def __init__(self, path) -> None:
+        self.tokenizer = get_tokenizer('basic_english')
+if __name__ == '__main__':
+    print("Prepare dataset...")
 
